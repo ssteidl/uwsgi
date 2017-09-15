476403a0b8194b40d514150abcdae0e1cfc25c23
Shane Steidley <ssteidley@gnsoftwarelabs.com>
2017-09-15 15:05:03 -0500

    Added uwsgi::respond tcl command.

--- plugins/tcl/tcl.c
+++ plugins/tcl/tcl.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <tcl8.6/tcl.h>
 #include <uwsgi.h>
 
@@ -24,6 +25,57 @@ static int uwsgi_tcl_init(){
 
 already_initialized:
     return 1;
+}
+
+static int uwsgi_tclrespond(ClientData clientData, Tcl_Interp *interp,
+                             int objc, struct Tcl_Obj *const *objv) {
+    int tcl_error = TCL_OK;
+
+    if(objc != 4)
+    {
+        Tcl_WrongNumArgs(interp, objc, objv, "Exactly 3 arguments required: <response code> <content-type> <payload>");
+        return TCL_ERROR;
+    }
+
+    int status = 0;
+    int status_number_len = 0;
+    char* status_number_str = Tcl_GetStringFromObj(objv[1], &status_number_len);
+    tcl_error = Tcl_GetInt(interp, status_number_str, &status);
+    if(tcl_error)
+    {
+        Tcl_Obj* args[] = {Tcl_GetObjResult(interp)};
+        Tcl_Obj* result_msg = Tcl_Format(interp, "Response code is not an integer: %s\n", 1, args);
+        if(result_msg == NULL)
+        {
+            uwsgi_log("Internal error: [%s line %d]\n", __FILE__, __LINE__);
+            exit(1);
+        }
+
+        Tcl_SetObjResult(interp, result_msg);
+        return TCL_ERROR;
+    }
+
+    struct wsgi_request* wsgi_req = current_wsgi_req();
+    assert(wsgi_req);
+    int error = uwsgi_response_prepare_headers(wsgi_req, status_number_str, status_number_len);
+    assert(!error);
+
+    int content_type_len = 0;
+    char* content_type = Tcl_GetStringFromObj(objv[2], &content_type_len);
+
+    error = uwsgi_response_add_content_type(wsgi_req, content_type, content_type_len);
+    assert(!error);
+
+    int payload_len = 0;
+    char* payload = Tcl_GetStringFromObj(objv[3], &payload_len);
+
+    error = uwsgi_response_add_content_length(wsgi_req, payload_len);
+    assert(!error);
+
+    error = uwsgi_response_write_body_do(wsgi_req, payload, payload_len);
+    assert(!error);
+
+    return TCL_OK;
 }
 
 static void uwsgi_tcl_app() {
@@ -56,6 +108,11 @@ static void uwsgi_tcl_app() {
     	uwsgi_log("Error evaluating Tcl Script: '%s' [%s line %d]\n",
     		  Tcl_GetStringResult(utcl.interp), __FILE__, __LINE__);
     }
+
+
+    Tcl_Command respond_cmd_token = Tcl_CreateObjCommand(utcl.interp, "uwsgi::respond", uwsgi_tclrespond, NULL, NULL);
+    //TODO: Store the token somewhere.
+    (void)respond_cmd_token;
 }
 
 static int uwsgi_tcl_request(struct wsgi_request *wsgi_req) {
@@ -72,8 +129,6 @@ static int uwsgi_tcl_request(struct wsgi_request *wsgi_req) {
     }
 
     Tcl_Obj* response = Tcl_GetObjResult(utcl.interp);
-    uwsgi_response_prepare_headers(wsgi_req, "200 OK", 6);
-    uwsgi_response_add_header(wsgi_req, "Content-type", 12, "text/plain", 9);
     int content_length = 0;
     char* response_string = Tcl_GetStringFromObj(response, &content_length);
     uwsgi_log("response: %s, %d\n", response_string, content_length);
@@ -84,8 +139,8 @@ static int uwsgi_tcl_request(struct wsgi_request *wsgi_req) {
 
 
 static void uwsgi_tcl_after_request(struct wsgi_request *wsgi_req) {
+
     log_request(wsgi_req);
-    uwsgi_log("i am the example plugin after request function\n");
 }
 
 struct uwsgi_option uwsgi_tcl_options[] = {
