f279b3fbc2bd23d72b43cfe1a96bae82fb4a0330
Shane Steidley <ssteidley@gnsoftwarelabs.com>
2017-09-27 07:01:47 -0500

    * Added environment dictionary with most of the normal   cgi environment variables.
    * Added the tcl.ini buildconf file.

--- examples/tcl_uwsgi.tcl
+++ examples/tcl_uwsgi.tcl
@@ -1,7 +1,4 @@
+proc application {environ} {
 
-proc / {uwsgi} {
-
-    set payload {{"id": 71, "value": "uwsgi is awesome"}}
-    append payload "\n"
-    uwsgi::respond {201} {text/json} $payload
+    puts "dict: $environ"
 }
