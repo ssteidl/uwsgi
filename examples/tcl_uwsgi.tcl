4650b4d3fb5d4bf36f6d2c192a2c62cbc5bbedae
Shane Steidley <ssteidley@gnsoftwarelabs.com>
2017-10-13 20:47:04 -0500

    Added support for reading request body

--- examples/tcl_uwsgi.tcl
+++ examples/tcl_uwsgi.tcl
@@ -1,4 +1,23 @@
 proc application {environ} {
 
-    puts "dict: $environ"
+    set response_code {200}
+    set payload {}
+
+    set method [dict get $environ REQUEST_METHOD]
+
+    switch $method {
+
+	GET {
+	    set payload $environ
+	}
+	POST {
+	    set payload [uwsgi::request_body -1]
+	    append payload " -- [string length $payload]"
+	}
+	default {
+	    set response_code {405}
+	}
+    }
+
+    uwsgi::respond $response_code {text/plain} $payload
 }
